34f21c31c40f5e5907223ca2ca968fd9e8a1321c
Mike <Mikemountain.dev@gmail.com>
2021-01-19 11:57:36 +0200

    Add firebase

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,7 @@
+import {firebase} from './firebase.const';
+
 export const environment = {
   production: false,
-  apiUrl: ''
+  apiUrl: '',
+  firebase
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+import {firebase} from './firebase.const';
+
 export const environment = {
   production: true,
-  apiUrl: ''
+  apiUrl: '',
+  firebase
 };
