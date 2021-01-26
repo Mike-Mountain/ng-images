7d1e3c1952228b7cb60edfce6f30c20239efb5b2
Mike <Mikemountain.dev@gmail.com>
2021-01-26 18:07:07 +0200

    Create a mobile view for the primary navigation + fix some tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
 import {LayoutComponent} from './modules/core/components/layout/layout.component';
 import {SidebarComponent} from './modules/core/components/sidebar/sidebar.component';
@@ -9,7 +8,6 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
         CoreModule
       ],
       declarations: [
